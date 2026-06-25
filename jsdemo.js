7ccd2d7a60d8bdddc28a84d0c2f79db27c83575c
MileyS5 <Sprattmiley74@gmail.com>
2026-06-25 15:46:54 -0400

    Add personal information and fun fact to jsdemo.js

--- jsdemo.js
+++ jsdemo.js
@@ -1,1 +1,8 @@
 console.log("This is loaded from the external JS file");
+var name = "Miley";
+let school = "Central Piedmont Early College";
+var fun_fact = "My primary hobby is art and design";
+
+console.log(`Name: ${name}`);
+console.log(`School: ${school}`);
+console.log(`Fun Fact: ${fun_fact}`);
